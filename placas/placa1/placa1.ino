9525c95742f8dcde134ec7883a04938c1f8416c8
Guilherme-JSS <schutzgui2707@gmail.com>
2025-10-07 10:05:29 -0300

    Adicionando conecxão ao usuario

--- placas/placa1/placa1.ino
+++ placas/placa1/placa1.ino
@@ -1,22 +1,38 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 WiFiClient client;
+PubSubClient mqtt(client);
+
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
 
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+
+const String brokerUser = "";
+const String brokerPass = "";
 
 void setup() {
   Serial.begin(115200);
-  
-  CLient.begin(SSID, PASS);
+  WiFi.begin(SSID, PASS);
   Serial.println("Conectado a rede");
 
-  while(client.status() != WL_CONNECTED){
+  while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
+     }
 
-  }
-
+Serial.println("Conectado com sucesso!");
+mqtt.setServer(brokerURL.c_str(),brokerPort);
+String clientID = "S1-" ; // criando um id unico e aleatorio
+clientID += String(random(0xffff),HEX);
+while(mqtt.connect(clientID.c_str())  == 0){  // tentativa de conetar,  0 = não conectado, 1 = conectado
+  Serial.print(".");
+  delay(200);
+    
+ }
+   Serial.println("\nConectado ao broker!");
 }
 
 void loop() {
