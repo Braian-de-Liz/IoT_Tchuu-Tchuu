78f108b485eb12e9569e7673a16d1bf3dd96468f
Guilherme-JSS <schutzgui2707@gmail.com>
2025-10-28 09:21:14 -0300

    Adição da Placa do Guilherme

--- placas/PlacaGui/PlacaGui.ino
+++ placas/PlacaGui/PlacaGui.ino
@@ -1,14 +1,11 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include "env.h"
+#include <WiFiClientSecure.h>
 
-WiFiClient client;
+WiFiClientSecure client;
 PubSubClient mqtt(client);
 
-const String SSID = "FIESC_IOT_EDU";
-const String PASS = "8120gv08";
-
-const String brokerURL = "test.mosquitto.org";
-const int brokerPort = 1883;
 const String topic = "TchuuTchuu";
 
 const String brokerUser = "";
@@ -16,8 +13,9 @@ const String brokerPass = "";
 
 void setup() {
   Serial.begin(115200); // configura a placa para monstrar na tela 
-  WiFi.begin(SSID, PASS); //tenta conectar na rede
+  WiFi.begin(WIFI_SSID, WIFI_PASS); //tenta conectar na rede
   Serial.println("Conectado a rede");
+  client.setInsecure();
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
@@ -39,31 +37,21 @@ while(mqtt.connect(clientID.c_str())  == 0){  // tentativa de conetar,  0 = não
 void loop() {
   String mensagem = ""; // cria dentro do loop para recriala toda vez que rodar o programa
   if(Serial.available()> 0 ){  // checagem se tem palavras na "fila de espera", se tiver alguma palavra faça
-   mensagem = Serial.readStringUntil('\n');    // salva a palavra digitada / lê a mensagem do usuario
-    mensagem = "Guilherme:" + mensagem; 
+   mensagem = Serial.readStringUntil('/n');    // salva a palavra digitada / lê a mensagem do usuario
+    mensagem = "Guilherme :" + mensagem; 
   // Serial.print("A mensagem foi: ");
   // Serial.println(mensagem);
-    mqtt.publish("TchuuTchuu", mensagem.c_str());
+    mqtt.publish("TchuuTchuu2", mensagem.c_str());
   };  
 
   mqtt.loop();
 }
 
-void callback(char* topic, byte* payload, unsigned long length) {   // perguntar ao professor pq unsigned long ou inves de unsigned int
+void callback(char* topic, byte* payload, unsigned long length) {
   String MensagemRecebida = "";
-
-  for(int i = 0; i < length; i++) { //Pega cada letra de payload e junta na mensagem
+ for(int i = 0; i < length; i++) {
+  //Pega cada letra de payload e junta na mensagem
   MensagemRecebida += (char) payload[i];
  }
-  Serial.println(MensagemRecebida);
-
-  if (MensagemRecebida == "1"){ // "" Pois está lendo a string
-    digitalWrite(2,HIGH);
-    Serial.prinlt("Sensor Ligado")
-  }else{
-    digitalWrite(2,LOW); 
-    Serial.println("Sensor Desligado")
-  };
-
- 
+ Serial.println(MensagemRecebida);
 }
---
+++ placas/PlacaGui/env.h
@@ -0,0 +1,17 @@
+#ifndef ENV_H
+#define ENV_H
+
+#define WIFI_SSID "FIESC_IOT_EDU"
+#define WIFI_PASS "8120gv08"
+
+
+#define BROKER_URL "test.mosquitto.org"
+#define BROKER_PORT 1883
+// -------Login criado para cada usuario no HiveMQ -------
+#define BROKER_USR_ID ""
+#define BROKER_USR_PASS ""
+//    ----A partir daqui depende da placa----
+#define TOPIC_PRESENCE1  "Tchuu-Tchuu/S4/Presence1" // nome do topico
+
+
+#endif 
