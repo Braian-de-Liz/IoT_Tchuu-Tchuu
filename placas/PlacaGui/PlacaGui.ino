8acbc7d9925aef119940eed4b798437990fbd27c
Guilherme-JSS <schutzgui2707@gmail.com>
2025-11-04 10:15:53 -0300

    iniciação dos tópicos - guilherme

--- placas/PlacaGui/PlacaGui.ino
+++ placas/PlacaGui/PlacaGui.ino
@@ -3,60 +3,64 @@
 #include "env.h"
 #include <WiFiClientSecure.h>
 
-<<<<<<< HEAD:placas/PlacaGui/PlacaGui.ino
 WiFiClientSecure client;
 PubSubClient mqtt(client);
 
-const String topic = "TchuuTchuu";
+const int ledPin = 2;
 
-const String brokerUser = "";
-const String brokerPass = "";
 
 void setup() {
-  Serial.begin(115200); // configura a placa para monstrar na tela 
-  WiFi.begin(WIFI_SSID, WIFI_PASS); //tenta conectar na rede
+  Serial.begin(115200);              // configura a placa para monstrar na tela
+  WiFi.begin(WIFI_SSID, WIFI_PASS);  //tenta conectar na rede
   Serial.println("Conectado a rede");
   client.setInsecure();
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(200);
-     }
+  }
 
 
-Serial.println("Conectado com sucesso!");
-mqtt.setServer(brokerURL.c_str(),brokerPort);
-String clientID = "S1-" ; // criando um id unico e aleatorio
-clientID += String(random(0xffff),HEX);
-while(mqtt.connect(clientID.c_str())  == 0){  // tentativa de conetar,  0 = não conectado, 1 = conectado
-  Serial.print(".");
-  delay(200);
-    
- }
-   Serial.println("\nConectado ao broker!");
+  Serial.println("Conectado com sucesso!");
+  mqtt.setServer(BROKER_URL, BROKER_PORT);
+  String clientID = "S1-";  // criando um id unico e aleatorio
+  clientID += String(random(0xffff), HEX);
+
+  while (mqtt.connect(clientID.c_str()) == 0) {  // tentativa de conetar,  0 = não conectado, 1 = conectado
+    Serial.print(".");
+    delay(200);
+  }
+  mqtt.subcribe(Topic_S1_Iluminacao);
+  Serial.println("\nConectado ao broker!");
+  mqtt.setCallback(callback);
 }
 
+
+
 void loop() {
-  String mensagem = ""; // cria dentro do loop para recriala toda vez que rodar o programa
-  if(Serial.available()> 0 ){  // checagem se tem palavras na "fila de espera", se tiver alguma palavra faça
-   mensagem = Serial.readStringUntil('/n');    // salva a palavra digitada / lê a mensagem do usuario
-    mensagem = "Guilherme :" + mensagem; 
-  // Serial.print("A mensagem foi: ");
-  // Serial.println(mensagem);
+  String mensagem = "";                       // cria dentro do loop para recriala toda vez que rodar o programa
+  if (Serial.available() > 0) {               // checagem se tem palavras na "fila de espera", se tiver alguma palavra faça
+    mensagem = Serial.readStringUntil('/n');  // salva a palavra digitada / lê a mensagem do usuario
+    mensagem.trim();
+    // Serial.print("A mensagem foi: ");
+    // Serial.println(mensagem);
 
-    mqtt.publish(Ponte-H, mensagem.c_str());
-
-  };  
-
+    mqtt.publish(Topic_S1_Iluminacao, mensagem.c_str());
+  };
   mqtt.loop();
 }
 
-<<<<<<< HEAD:placas/PlacaGui/PlacaGui.ino
+
 void callback(char* topic, byte* payload, unsigned long length) {
+
   String MensagemRecebida = "";
- for(int i = 0; i < length; i++) {
-  //Pega cada letra de payload e junta na mensagem
-  
-  MensagemRecebida += (char) payload[i];
- }
- Serial.println(MensagemRecebida);
+  for (int i = 0; i < length; i++) {  //Pega cada letra de payload e junta na mensagem
+    MensagemRecebida += (char)payload[i];
+  }
+  Serial.println(MensagemRecebida);
+
+  if (topic == Topic_S1_Iluminacao && MensagemRecebida == "acender") {
+    digitalWrite(ledPin, HIGH);  // acender led
+  } else if (topic == Topic_S1_Iluminacao && MensagemRecebida == "apagar") {
+    digitalWrite(ledPin, LOW);
+  }
 }
--- placas/PlacaGui/env.h
+++ placas/PlacaGui/env.h
@@ -5,14 +5,19 @@
 #define WIFI_PASS "8120gv08"
 
 
-#define BROKER_URL "test.mosquitto.org"
-#define BROKER_PORT 1883
+#define BROKER_URL "hivemq.webclient.1762258808551"
+#define BROKER_PORT 8883
 // -------Login criado para cada usuario no HiveMQ -------
-#define BROKER_USR_ID ""
-#define BROKER_USR_PASS ""
+#define BROKER_USR_ID "Placa_1"
+#define BROKER_USR_PASS "Aa_11111"
+
 //    ----A partir daqui depende da placa----
 
-#define TOPIC_PRESENCE1  "Tchuu-Tchuu/S4/Presence1" // nome do topico
+// #define TOPIC_PRESENCE1  "Tchuu-Tchuu/S4/Presence1" // nome do topico
 
+#define Topic_S1_Temp "Tchuu-Tchuu/S1/Temp"
+#define Topic_S1_Umid  "Tchuu-Tchuu/S1/Umid"
+#define Topic_S1_Presenca "Tchuu-Tchuu/S1/Presenca"
+#define Topic_S1_Iluminacao "Tchuu-Tchuu/S1/Ilumina"
 
 #endif 
