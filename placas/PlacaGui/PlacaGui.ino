79dc3604a31c83d2816d2cdef67b9f7ae02a5419
Guilherme-JSS <schutzgui2707@gmail.com>
2025-11-06 08:16:59 -0300

    Organizando a placa 2

--- placas/PlacaGui/PlacaGui.ino
+++ placas/PlacaGui/PlacaGui.ino
@@ -4,13 +4,13 @@
 #include <WiFiClientSecure.h>
 #include <DHT.h>
 
-#define pinLDR 34   // o LRD, luz 
+#define pinLDR 34  // o LRD, luz
 
-#define pinDHT 4  // pino do DHT , sensor de temp 
-#define DHTTYPE DHT11 // DHT , sensor tem temp e umidade
+#define pinDHT 4       // pino do DHT , sensor de temp
+#define DHTTYPE DHT11  // DHT , sensor tem temp e umidade
 DHT dht(pinDHT, DHTTYPE);
 
-const byte ledPin = 2; 
+const byte ledPin = 2;
 
 WiFiClientSecure client;
 PubSubClient mqtt(client);
@@ -34,7 +34,7 @@ void setup() {
   }
 
   Serial.println("Conectado com sucesso!");
-  
+
   mqtt.setServer(BROKER_URL, BROKER_PORT);
   String clientID = "S1-";  // criando um id unico e aleatorio
   clientID += String(random(0xffff), HEX);
@@ -52,21 +52,20 @@ void setup() {
   mqtt.setCallback(callback);
 
 
- pinMode(TRIGGER_PIN, OUTPUT);
- pinMode(ECHO_PIN, INPUT);
- pinMode(ledPin,OUTPUT);
-
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
-long lerDistancia(){
+long lerDistancia() {
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGER_PIN, LOW);
   long duracao = pulseIn(ECHO_PIN, HIGH);
-  long distancia = duracao * 349.24 / 2 / 10000 ;
-return distancia;
+  long distancia = duracao * 349.24 / 2 / 10000;
+  return distancia;
 }
 void loop() {
 
@@ -84,46 +83,46 @@ void loop() {
   delay(500);
 
   // Publicação da Iluminação
- 
-  int luz = map(analogRead(pinLDR),0,4095,0,100);
-      if (luz > 50){
-        mqtt.publish(Topic_S1_Iluminacao,"Claro");
-      }else {
-        mqtt.publish(Topic_S1_Iluminacao, "Escuro");
-      }
+
+  int luz = map(analogRead(pinLDR), 0, 4095, 0, 100);
+  if (luz > 50) {
+    mqtt.publish(Topic_S1_Iluminacao, "Claro");
+  } else {
+    mqtt.publish(Topic_S1_Iluminacao, "Escuro");
+  }
 
   mqtt.loop();
   delay(500);
 
-// Publicação da Presença 
- long distancia = lerDistancia();
+  // Publicação da Presença
+  long distancia = lerDistancia();
 
- if (distancia < 50 ){
-  mqtt.publish(Topic_S1_Presenca, "Em rota de Colisão !!!");
- }else{
-  mqtt.publish(Topic_S1_Presenca, "Caminho Livre");
- }
+  if (distancia < 50) {
+    mqtt.publish(Topic_S1_Presenca, "Em rota de Colisão !!!");
+  } else {
+    mqtt.publish(Topic_S1_Presenca, "Caminho Livre");
+  }
+
+  mqtt.loop();
+  delay(500);
+
+
+
+  //  Publicação da temperatura
+
+  float t = dht.readTemperature();
+  if (t > 28) {
+    mqtt.publish(Topic_S1_Temp, "A temperatura está elevada");  //quente
+  } else {
+    mqtt.publish(Topic_S1_Temp, "Regular");  // normal
+  }
 
   mqtt.loop();
   delay(500);
 
 
-
-//  Publicação da temperatura 
-
- float t = dht.readTemperature();
- if (t > 28){
-   mqtt.publish(Topic_S1_Temp, "A temperatura está elevada"); //quente
- }else{
-  mqtt.publish(Topic_S1_Temp, "Regular"); // normal
- }
-
-  mqtt.loop();
-  delay(500);
-      
-
-// Publicação da Umidade
- float u = dht.readHumidity();
+  // Publicação da Umidade
+  float u = dht.readHumidity();
   mqtt.publish(Topic_S1_Umid, String(u).c_str());
 }
 
@@ -137,7 +136,7 @@ void callback(char* topic, byte* payload, unsigned long length) {
   }
   Serial.println(MensagemRecebida);
 
-   if (strcmp(topic, Topic_S1_Iluminacao) == 0 && MensagemRecebida == "Claro") {
+  if (strcmp(topic, Topic_S1_Iluminacao) == 0 && MensagemRecebida == "Claro") {
     digitalWrite(ledPin, HIGH);  // acender led
   } else if (strcmp(topic, Topic_S1_Iluminacao) == 0 && MensagemRecebida == "Escuro") {
     digitalWrite(ledPin, LOW);
--- placas/placa2/placa2.ino
+++ placas/placa2/placa2.ino
@@ -1,9 +1,61 @@
+#include <Wifi.h>
+#Qinclude < PubSubClient.h>
+#include "env.h"
+#include <WifiClientSecure.h>
+
+WifiClientSecure client;
+PubSubClient mqtt(client);
+
+
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);              // configura a placa para monstrar na tela
+  WiFi.begin(WIFI_SSID, WIFI_PASS);  //tenta conectar na rede
+  Serial.println("Conectado a rede");
+  client.setInsecure();
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(200);
+  }
+
+  Serial.println("Conectado com sucesso!");
+
+  mqtt.setServer(BROKER_URL, BROKER_PORT);
+  String clientID = "S1-";  // criando um id unico e aleatorio
+  clientID += String(random(0xffff), HEX);
+
+  while (mqtt.connect(clientID.c_str()) == 0) {  // tentativa de conetar,  0 = não conectado, 1 = conectado
+    Serial.print(".");
+    delay(200);
+  }
+
+  mqtt.subscribe(Topic_S2_Presenca1);
+  mqtt.subscribe(Topic_S2_Presenca2);
+
+  Serial.println("\nConectado ao broker!");
+  mqtt.setCallback(callback);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // 
+}
 
+
+void callback(char* topic, byte* payload, unsigned long length) {
+  String MensagemRecebida = "";
+
+  for (int = 0; i < length; i++) {
+    MensagemRecebida += (char)payload[i];
+  }
+  Serial;
+  println(MensagemRecebida);
+
+  if (strcmp(topic, Topic_S1_Iluminacao) == 0 && MensagemRecebida == "Claro") {
+    digitalWrite(ledPin, HIGH);  // acender led
+  } else if (strcmp(topic, Topic_S1_Iluminacao) == 0 && MensagemRecebida == "Escuro") {
+    digitalWrite(ledPin, LOW);
+  }
 }
